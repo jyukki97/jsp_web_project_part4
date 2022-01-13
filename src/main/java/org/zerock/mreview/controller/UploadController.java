3a35e6c83235d1a72e66200e18d686c95e7bbcca
jyukki97 <33771570+jyukki97@users.noreply.github.com>
2022-01-13 14:31:18 +0900

    jsp_web_project_part4-15 [View] 업로드 결과 반환과 화면 처리

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -2,16 +2,27 @@ package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,12 +32,28 @@ public class UploadController {
     @Value("${org.zerock.upload.path}")
     private String uploadPath;
 
+    private String makeFolder() {
+        String str = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if(!uploadPathFolder.exists()) {
+            uploadPathFolder.mkdirs();
+        }
+        return folderPath;
+    }
+
+
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) {
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
+
         for(MultipartFile uploadFile: uploadFiles) {
-            if(uploadFile.getContentType().startsWith("image") == false) {
+            if(!uploadFile.getContentType().startsWith("image")) {
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             String originalName = uploadFile.getOriginalFilename();
@@ -44,22 +71,31 @@ public class UploadController {
 
             try {
                 uploadFile.transferTo(savePath);
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
-    private String makeFolder() {
-        String str = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
 
-        String folderPath = str.replace("/", File.separator);
+        try {
+            String srcFileName = URLDecoder.decode(fileName, StandardCharsets.UTF_8);
+            log.info("fileName: " + srcFileName);
 
-        File uploadPathFolder = new File(uploadPath, folderPath);
+            File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file: " + file);
 
-        if(uploadPathFolder.exists() == false) {
-            uploadPathFolder.mkdirs();
+            HttpHeaders header = new HttpHeaders();
+
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            return new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return folderPath;
     }
 }
